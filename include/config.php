ab7493bbc877c766a756eb8c78bda055124d4806
trevorbennett <tb684611@ohio.edu>
2015-02-09 20:35:54 -0500

    config.php added

---
+++ include/config.php
@@ -0,0 +1,11 @@
+<?php
+define("HOST", "localhost");
+define("USER", "ohiothet_user");
+define("PASSWORD", "IF YOU COMMIT THIS FILE WITH A REAL PASSWORD I WILL DISOWN YOU"); 
+define("DATABASE", "ohiothet_actives");
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // https flag
+?>
