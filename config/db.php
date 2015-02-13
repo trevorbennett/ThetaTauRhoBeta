d0a7ae3bf4c50db88587f9880ad41f33147f0de6
trevorbennett <tb684611@ohio.edu>
2015-02-13 10:21:06 -0500

    Login system completed

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+Note: only change password ON THE SERVER
+ */
+define("DB_HOST", "localhost");
+define("DB_NAME", "ohiothet_actives");
+define("DB_USER", "ohiothet_user");
+define("DB_PASS", "DO NOT EVER PUSH THE PASSWORD OUT TO GIT");
