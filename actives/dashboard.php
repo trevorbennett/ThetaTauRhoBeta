6566f7b426da1c5a83e5fa056b2079609dd3792e
yonryzhu <yz183712@ohio.edu>
2015-03-28 23:14:45 -0400

    made a dash page

---
+++ actives/dashboard.php
@@ -0,0 +1,80 @@
+<!DOCTYPE HTML>
+<!--
+	Twenty by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+        <link rel="icon" href="http://engineering.jhu.edu/thetatau/img/favicon.ico" type="image/x-icon">
+		<title>Mission | Theta Tau | Rho Beta</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery.dropotron.min.js"></script>
+		<script src="js/jquery.scrolly.min.js"></script>
+		<script src="js/jquery.scrollgress.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-wide.css" />
+			<link rel="stylesheet" href="css/style-noscript.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+	</head>
+	<body class="no-sidebar">
+
+		<!-- Header -->
+			<header id="header" class="skel-layers-fixed">
+				<h1 id="logo"><a href="welcome">Theta Tau <span>Rho Beta</span></a></h1>
+				<nav id="nav">
+					<ul>
+						<li class="current"><a href="welcome">Welcome</a></li>
+						<li class="submenu">
+							<a href="">Menu</a>
+							<ul>
+								<li><a href="login">Actives</a></li>
+								<li><a href="unavailable">Rush</a></li>
+								<li><a href="unavailable">Alumni</a></li>
+								<li><a href="unavailable">Contact</a></li>
+                                <li><a href="./email">Email Sign In</a></li>
+								<li class="submenu">
+									<a href="unavailable">About</a>
+																		<ul>
+
+										<li><a href="exec">Executive Board</a></li>                                  
+										<li><a href="members">Members</a></li>
+										<li><a href="mission">Mission</a></li>
+										<li><a href="unavailable">News</a></li>
+										<li><a href="history">History</a></li>
+									</ul>
+								</li>
+							</ul>
+						</li>
+						<li><a href="login" class="button special">Login</a></li>
+					</ul>
+				</nav>
+			</header>
+
+<!-- Footer -->
+			<footer id="footer">
+
+				<ul class="icons">
+					<li><a href="https://www.facebook.com/groups/368647956546433" class="icon circle fa-facebook"><span class="label">Facebook</span></a></li>
+					<li><a href="https://twitter.com/ThetaTauOU" class="icon circle fa-twitter"><span class="label">Twitter</span></a></li>
+					<li><a href="https://www.linkedin.com/groups?home=&gid=4873915" class="icon circle fa-linkedin"><span class="label">LinkedIn</span></a></li>
+				</ul>
+
+				<ul class="copyright">
+					<li>&copy; Theta Tau Rho Beta</li><li>Template by: <a href="http://html5up.net">HTML5 UP</a></li>
+				</ul>
+
+			</footer>
+	</body>
+</html>
